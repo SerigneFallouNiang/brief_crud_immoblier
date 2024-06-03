6fa04ffbf092163cd56a9b6652339a61f4121d7b
SerigneFallouNiang <fallouniang776@gmail.com>
2024-06-03 14:06:28 +0000

    feat: affichage du formulaire d'inscription

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::controller(AuthController::class)->group(function(){
+
+    Route::get('/inscription','inscription')->name('inscription');
+    Route::post('/inscription','inscription')->name('inscription');
+
+
 });
